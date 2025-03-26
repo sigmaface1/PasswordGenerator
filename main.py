dae4ffcfbaafb7981135b0cbb4163ad6118d4438
Даниил <misataaas@gmail.com>
2025-03-26 13:29:44 +0300

    better code

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import random
 import string
 
 
-char_list = list(string.ascii_letters + string.digits)
-print(len(char_list))
+def generate_password():
+    char_list = list(string.ascii_letters + string.digits)
+    char_list_len = len(char_list)
 
-password = []
-i = int(input("Количество символов: "))
-b = i
+    password = []
+    symbols_count = int(input("Количество символов: "))
+    symbols_count_2 = symbols_count
 
-while i < b * 2 :
-    num = random.randrange(0, 62 )
-    password += char_list[num]
-    i += 1
+    while symbols_count > 0:
+        num = random.randrange(0, char_list_len)
+        password += char_list[num]
+        symbols_count -= 1
 
-print("".join(password))
+    print("".join(password))
+
+if __name__ == "__main__":
+    generate_password()
